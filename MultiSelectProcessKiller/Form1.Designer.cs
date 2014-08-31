f4aa58c4a6e3d4b64f72976cc54035423d5fb2d1
Ottak <michael.catto@gmail.com>
2014-08-31 13:12:42 -0400

    Initial Commit Multi Process Killer
    Avoid task manager one-by-one kill with multi select

---
+++ MultiSelectProcessKiller/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace ProcessKiller
+{
+    partial class frmMainnnnnn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgProcs = new System.Windows.Forms.DataGridView();
+            this.Kill = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Responding = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Threads = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Handles = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TotalTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PagedMem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NonPageMem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ckSafeMode = new System.Windows.Forms.CheckBox();
+            this.btnEditConfig = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProcs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(13, 594);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kill Selected";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgProcs
+            // 
+            this.dgProcs.AllowUserToAddRows = false;
+            this.dgProcs.AllowUserToDeleteRows = false;
+            this.dgProcs.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Wheat;
+            this.dgProcs.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgProcs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgProcs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProcs.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Kill,
+            this.Name,
+            this.PID,
+            this.Responding,
+            this.Threads,
+            this.Handles,
+            this.TotalTime,
+            this.PagedMem,
+            this.NonPageMem});
+            this.dgProcs.Location = new System.Drawing.Point(2, -1);
+            this.dgProcs.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgProcs.Name = "dgProcs";
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Cornsilk;
+            this.dgProcs.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgProcs.Size = new System.Drawing.Size(768, 585);
+            this.dgProcs.TabIndex = 1;
+            // 
+            // Kill
+            // 
+            this.Kill.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Kill.HeaderText = "Kill";
+            this.Kill.Name = "Kill";
+            this.Kill.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Kill.Width = 63;
+            // 
+            // Name
+            // 
+            this.Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Name.HeaderText = "Exe";
+            this.Name.Name = "Name";
+            // 
+            // PID
+            // 
+            this.PID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.PID.HeaderText = "PID";
+            this.PID.Name = "PID";
+            this.PID.Width = 69;
+            // 
+            // Responding
+            // 
+            this.Responding.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
+            this.Responding.HeaderText = "Alive";
+            this.Responding.Name = "Responding";
+            this.Responding.Width = 5;
+            // 
+            // Threads
+            // 
+            this.Threads.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Threads.HeaderText = "Threads";
+            this.Threads.Name = "Threads";
+            this.Threads.Width = 110;
+            // 
+            // Handles
+            // 
+            this.Handles.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Handles.HeaderText = "Handles";
+            this.Handles.Name = "Handles";
+            this.Handles.Width = 109;
+            // 
+            // TotalTime
+            // 
+            this.TotalTime.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.TotalTime.HeaderText = "TotalTime";
+            this.TotalTime.Name = "TotalTime";
+            this.TotalTime.Width = 125;
+            // 
+            // PagedMem
+            // 
+            this.PagedMem.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.PagedMem.HeaderText = "PagedMem";
+            this.PagedMem.Name = "PagedMem";
+            this.PagedMem.Width = 138;
+            // 
+            // NonPageMem
+            // 
+            this.NonPageMem.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.NonPageMem.HeaderText = "NonPageMem";
+            this.NonPageMem.Name = "NonPageMem";
+            this.NonPageMem.Width = 163;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(485, 594);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ckSafeMode
+            // 
+            this.ckSafeMode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ckSafeMode.AutoSize = true;
+            this.ckSafeMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckSafeMode.Location = new System.Drawing.Point(156, 601);
+            this.ckSafeMode.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ckSafeMode.Name = "ckSafeMode";
+            this.ckSafeMode.Size = new System.Drawing.Size(158, 24);
+            this.ckSafeMode.TabIndex = 3;
+            this.ckSafeMode.Text = "Prompt Yes/No? ";
+            this.ckSafeMode.UseVisualStyleBackColor = true;
+            // 
+            // btnEditConfig
+            // 
+            this.btnEditConfig.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEditConfig.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditConfig.Location = new System.Drawing.Point(327, 594);
+            this.btnEditConfig.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEditConfig.Name = "btnEditConfig";
+            this.btnEditConfig.Size = new System.Drawing.Size(150, 31);
+            this.btnEditConfig.TabIndex = 4;
+            this.btnEditConfig.Text = "Edit Config";
+            this.btnEditConfig.UseVisualStyleBackColor = true;
+            this.btnEditConfig.Click += new System.EventHandler(this.btnEditConfig_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(605, 594);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 31);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMainnnnnn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 633);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnEditConfig);
+            this.Controls.Add(this.ckSafeMode);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dgProcs);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            //this.Name = "frmMainnnnnn";
+            this.Opacity = 0.8D;
+            this.ShowIcon = false;
+            this.Text = "Multi  Select  Process  Killer";
+            ((System.ComponentModel.ISupportInitialize)(this.dgProcs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgProcs;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox ckSafeMode;
+        private System.Windows.Forms.Button btnEditConfig;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Exe;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Kill;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Responding;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Threads;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Handles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TotalTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PagedMem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NonPageMem;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ MultiSelectProcessKiller/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Configuration;
+
+
+namespace ProcessKiller
+{
+    public partial class frmMainnnnnn : Form
+    {
+        public frmMainnnnnn()
+        {
+            InitializeComponent();
+            InitStuff();
+            InitGrid();
+        }
+
+
+        //string thingsToKill = ",AdobeUpdater,cisvc,GoogleUpdate,VCDDaemon,jucheck,jusched,stsystra,";
+        string thingsToKill = "fgghfghfghfghfgh";
+        string whatToEditConfigWith = "fgghfghfghfghfgh";
+        int debug = 0;
+        bool safeMode = true;
+        const string ckSafeModeText = "Prompt Yes/No for each delete? ";
+        const int colKill  = 0;
+        const int colName  = 1;
+        const int colPID =2;
+        const int colRespond = 3;
+        const int colThreads = 4;
+        const int colModules = 5;
+        const int colStartTime = 6;
+        const int colPagedMem = 7;
+        const int colNonPagedMem = 8;
+        System.IO.FileSystemWatcher watchConfig;
+
+       public void InitStuff()
+        {
+            //read App.Config
+            System.Configuration.AppSettingsReader rdr = new System.Configuration.AppSettingsReader();
+            thingsToKill = (string)rdr.GetValue("thingsToKill", thingsToKill.GetType());
+            whatToEditConfigWith = (string)rdr.GetValue("whatToEditConfigWith", whatToEditConfigWith.GetType());
+            debug = (int)rdr.GetValue("debug", debug.GetType());
+            safeMode = (bool)rdr.GetValue("safeMode", safeMode.GetType());
+
+            //SET up file watcher to restart EXE upon config change
+            //string path = Process.GetCurrentProcess().MainModule.FileName;
+            //path = path.Replace("vshost.exe", "exe.config");
+            //watchConfig = new System.IO.FileSystemWatcher(path);
+            watchConfig = new System.IO.FileSystemWatcher(".", "ProcessKiller.exe.config");
+            watchConfig.NotifyFilter = System.IO.NotifyFilters.LastWrite;
+            watchConfig.Changed += new System.IO.FileSystemEventHandler(configChanged);
+            watchConfig.EnableRaisingEvents = true;
+        }
+
+       private static void configChanged(object source, System.IO.FileSystemEventArgs e)
+       {
+           //  Show that a file has been created, changed, or deleted.
+           System.IO.WatcherChangeTypes wct = e.ChangeType;
+           
+           //config changed, start ne process then kill me
+           Process killer = new Process();
+           killer.StartInfo.FileName = "ProcessKiller.exe";
+           killer.Start();
+
+           Process.GetCurrentProcess().Kill();   
+       }
+   
+
+        public void KillProcessesFromConfig()
+        {
+            string pName = "";
+            string pNames = "";
+            string killedpNames = "";
+
+            if (debug == 1)
+            {
+                MessageBox.Show(this, "The kill list ..... \n\n " + thingsToKill);
+            }
+            //3.5 framework
+            //foreach (Process opro in Process.GetProcesses().OrderBy(p => p.ProcessName))
+                
+            //2.0 framework
+            foreach (Process opro in Process.GetProcesses(   ))
+            {
+                pName = opro.ProcessName ;
+                pNames += opro.ProcessName + "\t\t" + opro.Id + "\t\t" + opro.VirtualMemorySize + "\t\t" + opro.PrivateMemorySize + "\n";
+                //pNames += opro.ProcessName + "\n";
+
+                if (thingsToKill.Contains("," + pName + ",") == true)
+                {
+                    killedpNames += opro.ProcessName + "\n";
+                    if (debug == 1)
+                    {
+                        MessageBox.Show(this, "Killing ......  " + pName);
+                    }
+                    opro.Kill(); 
+                }
+
+            }
+
+            if (debug == 1)
+            {
+                MessageBox.Show(this, "All the processes found ..... \n\n " + pNames);
+            }
+
+            MessageBox.Show(this, "The things killed ..... \n\n " + killedpNames);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.KillProcesses();
+        }
+
+
+
+        public void InitGrid()
+        {
+            string pName = "";
+            string pNames = "";
+            int debug = 0;
+            object[] rparams = new object[9];
+
+            if (safeMode == true)
+            {
+                ckSafeMode.Checked = true;
+                ckSafeMode.Enabled = false;
+                ckSafeMode.Text = ckSafeModeText + " (Change App.Config value to false to enable choice.) ";
+            }
+
+            dgProcs.Rows.Clear();
+
+            if (debug == 1)
+            {
+                MessageBox.Show(this, "The kill list ..... \n\n " + thingsToKill);
+            }
+            //3.5 framework
+            //foreach (Process opro in Process.GetProcesses().OrderBy(p => p.ProcessName))
+
+            //2.0 framework
+            //foreach (Process opro in Process.GetProcesses()  )
+            foreach (Process opro in Process.GetProcesses().OrderBy(p => p.ProcessName))
+            {
+                pName = opro.ProcessName;
+                pNames += opro.ProcessName + "\t\t" + opro.Id + "\t\t" + opro.VirtualMemorySize + "\t\t" + opro.PrivateMemorySize + "\n";
+                //pNames += opro.ProcessName + "\n";
+
+                rparams[colName] = opro.ProcessName;
+                rparams[colPID] = opro.Id;
+                rparams[colKill] = false;
+                rparams[colPagedMem] = opro.PagedMemorySize64.ToString();
+                rparams[colNonPagedMem] = opro.NonpagedSystemMemorySize64.ToString();
+                rparams[colRespond] = opro.Responding;
+                rparams[colThreads] = opro.Threads.Count;
+                rparams[colModules] = opro.HandleCount;
+              
+
+                try { rparams[colStartTime] = opro.TotalProcessorTime.ToString(); }
+                catch { rparams[colStartTime] = "Ex"; }
+
+                if (thingsToKill.Contains("," + pName + ",") == true)
+                {
+                    rparams[colKill] = true;
+                }
+
+                dgProcs.Rows.Add(rparams);
+            }
+
+            if (debug == 1)
+            {
+                MessageBox.Show(this, "All the processes found ..... \n\n " + pNames);
+            }
+
+            dgProcs.Sort(dgProcs.Columns[colKill], ListSortDirection.Descending);
+            //dgProcs.Sort(dgProcs.Columns[0], ListSortDirection.Ascending);
+            
+        }
+
+        public void KillProcesses()
+        {
+            try
+            {
+                DialogResult response;
+
+                for (int i = 0; i < dgProcs.Rows.Count - 1; i++)
+                {
+                    if ((bool)dgProcs.Rows[i].Cells[colKill].Value == true)
+                    {
+                        if ((safeMode == true) || (ckSafeMode.Checked == true))
+                        {
+                            response = MessageBox.Show(this, "Killing ......  " + (string)dgProcs.Rows[i].Cells[colName].Value, "Killing", MessageBoxButtons.YesNo);
+                            if (response == DialogResult.Yes)
+                            {
+                                System.Diagnostics.Process.GetProcessById((int)dgProcs.Rows[i].Cells[colPID].Value).Kill();
+                            }
+                        }
+                        else
+                        {
+                            if (debug == 1) { MessageBox.Show(this, "Killing ......  " + (string)dgProcs.Rows[i].Cells[colName].Value); }
+                            System.Diagnostics.Process.GetProcessById((int)dgProcs.Rows[i].Cells[colPID].Value).Kill();
+                        }
+                    }
+                }
+                System.Threading.Thread.Sleep(1000);
+                InitGrid();
+            }
+            catch (Exception ex)
+            {
+                InitGrid();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InitStuff();
+            InitGrid();
+        }
+
+        private void btnEditConfig_Click(object sender, EventArgs e)
+        {
+            //quick and dirty, full path to wherever the exe is running....replacing ending with config ending
+            string configFile = Process.GetCurrentProcess().MainModule.FileName;
+            string configFile2 = configFile.Replace(".exe", ".exe.config");
+            if (debug == 1)
+            {
+                MessageBox.Show(this, "Config  .....  " + configFile + "\n\nConfig after replace .....  " + configFile2);
+            }
+
+            //defualt is notepad.exe.....can be textpad, wordpad, xmlspy, whatever..
+            Process notePad = new Process();
+            notePad.StartInfo.FileName =  whatToEditConfigWith;
+            //notePad.StartInfo.Arguments = Process.GetCurrentProcess().ProcessName + ".exe.config";
+            notePad.StartInfo.Arguments = '"' + configFile2 + '"';
+            notePad.Start();
+        }
+
+
+        private int GetTotalCPU()
+        {
+            int _retVal = 0;
+            foreach (Process opro in Process.GetProcesses().OrderBy(p => p.ProcessName))
+            {
+                
+            }
+            return _retVal;
+        }
+
+  
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();   
+
+        }
+
+    }
+}
